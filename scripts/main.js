050962f0d96f804e5ccb8e7d18d8d0d5c83e13e5
max-t-smith <maxtuerkheimersmith@gmail.com>
2024-08-04 21:30:11 -0500

    finishing touches for unit 7 lab

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,63 @@
+
+var beachIcon = L.icon({
+    iconUrl: 'pics/beach_icon.webp',
+    
+
+    iconSize:     [60, 60], // size of the icon
+    iconAnchor:   [30, 30], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
+});
+
+var boatIcon = L.icon({
+    iconUrl: 'pics/boat_icon.png',
+    
+
+    iconSize:     [40, 40], // size of the icon
+    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
+});
+
+var dogIcon = L.icon({
+    iconUrl: 'pics/dog_icon.png',
+    
+
+    iconSize:     [40, 40], // size of the icon
+    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
+});
+
+var sailboatIcon = L.icon({
+    iconUrl: 'pics/sailboat_icon.png',
+    
+
+    iconSize:     [40, 40], // size of the icon
+    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
+    popupAnchor:  [0, -30] // point from which the popup should open relative to the iconAnchor
+});
+
+var map = L.map('map').setView([42.04560, -87.67232], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 19,
+attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var lighthouse = L.marker([42.0640, -87.6744], {icon:beachIcon}).addTo(map);
+lighthouse.bindPopup("Lighthouse Beach");
+var clark = L.marker([42.0493, -87.6728], {icon:beachIcon}).addTo(map);
+clark.bindPopup("Clark Street Beach");
+var dog_beach = L.marker([42.0470, -87.6724], {icon:dogIcon}).addTo(map);
+dog_beach.bindPopup("Dog Beach")
+var church = L.marker([42.04560, -87.67232], {icon:boatIcon}).addTo(map);
+church.bindPopup("Boat Ramp")
+var greenwood = L.marker([42.0432, -87.6714], {icon:beachIcon}).addTo(map);
+greenwood.bindPopup("Greenwood Street Beach")
+var dempster = L.marker([42.0408, -87.6696], {icon:sailboatIcon}).addTo(map);
+dempster.bindPopup("Dempster Street Sailing Beach + Beach Office");
+var lee = L.marker([42.0363, -87.6699], {icon:beachIcon}).addTo(map);
+lee.bindPopup("Lee Street Beach");
+var south = L.marker([42.0275, -87.6678], {icon:beachIcon}).addTo(map);
+south.bindPopup("South Boulevard Beach");
+
+
 function displayElement(id)
 {
     document.getElementById(id).style.display = "block";
@@ -25,4 +85,4 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll('input[type="checkbox"][toggle-content]').forEach(checkbox => {
         toggleContent(checkbox);
     });
-});
+    });
